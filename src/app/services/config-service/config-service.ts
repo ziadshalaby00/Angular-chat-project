00450f458c255ea44db380476073e10591754f16
ziad <shalabyziad94@gmail.com>
2026-05-01 20:29:21 +0300

    update

--- src/app/services/config-service/config-service.ts
+++ src/app/services/config-service/config-service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class ConfigService {
   private readonly isProd: boolean = false;
 
-  private readonly localApiUrl: string = 'http://localhost:8000';
+  private readonly localApiUrl: string = '';
   private readonly prodApiUrl: string = 'https://api.example.com';
 
-  private readonly localWsProtocol: string = 'ws'
-  private readonly prodWsProtocol: string = 'wss'
+  private readonly localWsProtocol: string = 'ws';
+  private readonly prodWsProtocol: string = 'wss';
 
-  private readonly localSocketUrl: string = 'localhost:8000'
-  private readonly prodSocketUrl: string = 'api.example.com'
-  
-  public get WsProtocol(): string {
-    return this.isProd ? this.prodWsProtocol : this.localWsProtocol;
-  }
+  private readonly localSocketUrl: string = window.location.host;
+  private readonly prodSocketUrl: string = 'api.example.com';
 
   public get apiUrl(): string {
     return this.isProd ? this.prodApiUrl : this.localApiUrl;
+  }
+
+  public get WsProtocol(): string {
+    return this.isProd ? this.prodWsProtocol : this.localWsProtocol;
   }
 
   public get socketUrl(): string {
