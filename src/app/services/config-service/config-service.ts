8f76818050e52285a25399fcba1c46aac8f4969f
ziad <shalabyziad94@gmail.com>
2026-05-02 02:46:34 +0300

    update

--- src/app/services/config-service/config-service.ts
+++ src/app/services/config-service/config-service.ts
@@ -5,13 +5,13 @@ export class ConfigService {
   private readonly isProd: boolean = !window.location.hostname.includes('localhost');
 
   private readonly localApiUrl: string = 'http://localhost:8000';
-  private readonly prodApiUrl: string = 'https://ziadshalaby00.pythonanywhere.com/';
+  private readonly prodApiUrl: string = '';
 
   private readonly localWsProtocol: string = 'ws';
   private readonly prodWsProtocol: string = 'wss';
 
   private readonly localSocketUrl: string = 'localhost:8000';
-  private readonly prodSocketUrl: string = 'ziadshalaby00.pythonanywhere.com';
+  private readonly prodSocketUrl: string = '';
 
   public get apiUrl(): string {
     return this.isProd ? this.prodApiUrl : this.localApiUrl;
