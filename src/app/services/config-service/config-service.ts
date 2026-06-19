33493e1a6032d1396205dfed1b689dfa71ab084d
ziad <shalabyziad94@gmail.com>
2026-06-19 23:45:26 +0300

    update

--- src/app/services/config-service/config-service.ts
+++ src/app/services/config-service/config-service.ts
@@ -5,7 +5,7 @@ export class ConfigService {
   private readonly isProd: boolean = !window.location.hostname.includes('localhost');
 
   private readonly localApiUrl: string = 'http://localhost:8000';
-  private readonly prodApiUrl: string = 'https://women-quit-operation-conflict.trycloudflare.com';
+  private readonly prodApiUrl: string = 'https://shows-toe-requesting-sheriff.trycloudflare.com';
 
   private readonly localWsProtocol: string = 'ws';
   private readonly prodWsProtocol: string = 'wss';
